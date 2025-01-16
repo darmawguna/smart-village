a5b2288e9a5fd84ea2805f316586a9306b0d71db
Wahyu-Harta-Sasmita <wahyusasmitaharta561@gmail.com>
2025-01-16 18:46:12 +0800

    test

--- resources/js/Pages/Product.jsx
+++ resources/js/Pages/Product.jsx
@@ -3,11 +3,21 @@ import SearchBar from "@/Components/Elements/Search";
 import CardBanner from "@/Components/Fragments/CardBanner";
 import CardProduct from "@/Components/Fragments/CardProduct";
 import HomeLayout from "@/Layouts/HomeLayout";
-import React from "react";
+import React, { useState } from "react";
 
 const Product = (props) => {
-    const { produk, storageBaseUrl } = props
-    const dataProduk = produk.data
+    const { produk, storageBaseUrl } = props;
+    const dataProduk = produk.data;
+    const [searchTerm, setSearchTerm] = useState('');
+    
+      const handleSearchChange = (event) => {
+        setSearchTerm(event.target.value);
+      };
+    
+      const filteredArtikel = artikel.data.filter((item) =>
+        item.judul.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+      
     return (
         <HomeLayout>
             <div className="flex justify-center min-h-screen mt-[80px] px-4 ">
@@ -18,7 +28,9 @@ const Product = (props) => {
                                 Jelajahi Hasil Alam Terbaik
                             </h1>
                             <ButtonNavigate variant="w-[200px] rounded-lg px-2 py-2 text-center bg-white flex justify-between mt-2 items-center">
-                                <span className="text-black">Belanja Sekarang</span>
+                                <span className="text-black">
+                                    Belanja Sekarang
+                                </span>
                                 <img
                                     src="/assets/icons/next.png"
                                     alt="next"
@@ -42,24 +54,26 @@ const Product = (props) => {
                     </div>
                     <div className="flex justify-center sm:justify-end mt-7">
                         {/* TODO: perbarui sidebar ini untuk menyimpan data di state */}
-                        <SearchBar variant='w-[500px] sm:w-[300px]' placeholder='cari produk lainnya disini' />
+                        <SearchBar
+                            variant="w-[500px] sm:w-[300px]"
+                            placeholder="cari produk lainnya disini"
+                            value={searchTerm}
+                            onChange={handleSearchChange}
+                        />
                     </div>
                     <div className="flex py-10 flex-wrap justify-center gap-5">
-                        {
-                            dataProduk.map((data, index) => {
-                                return (
-                                    <CardProduct
-                                        key={data.id}
-                                        prodImages={`${ storageBaseUrl }/${ data.gambar_produk }`}
-                                        prodName={data.nama}
-                                        prodPrice={data.harga}
-                                        prodStock="1"
-                                        prodId={data.id}
-                                    >
-                                    </CardProduct>
-                                )
-                            })
-                        }
+                        {dataProduk.map((data, index) => {
+                            return (
+                                <CardProduct
+                                    key={data.id}
+                                    prodImages={`${storageBaseUrl}/${data.gambar_produk}`}
+                                    prodName={data.nama}
+                                    prodPrice={data.harga}
+                                    prodStock="1"
+                                    prodId={data.id}
+                                ></CardProduct>
+                            );
+                        })}
 
                         {/* <CardProduct prodImages="/assets/images/produ1.png" />
                     <CardProduct prodImages="/assets/images/produ1.png" />
