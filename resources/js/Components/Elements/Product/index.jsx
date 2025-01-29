3d17aab836d52bd0686ab0944971edd17b6ee7fc
Wahyu-Harta-Sasmita <wahyusasmitaharta561@gmail.com>
2025-01-29 10:57:49 +0800

    card product

--- resources/js/Components/Elements/Product/index.jsx
+++ resources/js/Components/Elements/Product/index.jsx
@@ -1,18 +1,14 @@
 import React from 'react'
 
 const ProductCard = (props) => {
-    const { nama, jumlah, 
-        // satuan,
+    const { nama, 
          harga } = props;
     
     return (
-        <div>
-            <h1>{nama}</h1>
-            <h2>stok: {jumlah}</h2>
-            <h3>{harga}
-                 {/* / {satuan} */}
-            </h3>
-        </div>
+        <div class="flex flex-col justify-between h-[100px]">
+    <h1>{nama}</h1>
+    <h3>{harga}</h3>
+</div>
     );
 }
 
